bf30364d70fbf18c540a7b9b24c0e363c0b64964
lisiyu06 <18392609095@163.com>
2019-11-03 21:25:32 +0800

    栈的实现

---
+++ Stack.java
@@ -0,0 +1,52 @@
+package homework19;
+
+/**
+ * Author: lisiyu
+ * Created: 2019/11/3
+ */
+
+// 实现一个基础的栈
+
+public class Stack {
+    // 使用一个数组来表示栈
+    // 先不考虑扩容
+    private int[] array = new int[100];
+    private int size = 0;
+
+    // 栈的核心操作
+
+    // 1. 入栈
+    public void push(int x) {
+        array[size] = x;
+        size++;
+    }
+
+    // 2. 出栈
+    public Integer pop() {
+        if (size == 0) {
+            return null;
+        }
+        int ret = array[size - 1];
+        size--;
+        return ret;
+    }
+
+    // 3. 取栈顶元素
+    public Integer peek() {
+        if (size == 0) {
+            return null;
+        }
+        return array[size - 1];
+    }
+
+    // 4. 判定栈为空
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // 5. 栈的大小
+    public int size() {
+        return size;
+    }
+
+}
---
+++ Test.java
@@ -0,0 +1,15 @@
+package homework19;
+
+import java.util.Stack;
+
+/**
+ * Author: lisiyu
+ * Created: 2019/11/3
+ */
+public class Test {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        Integer ret = stack.pop();
+        System.out.println(ret);
+    }
+}
